c9f6a011a4e8c993402049aecf42a717adde36ee
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-04-03 16:11:56 +0000

    Added gravatar support ...

---
+++ src/helpers/gravatars/GravatarsHelper.php
@@ -0,0 +1,27 @@
+<?php
+namespace ntentan\extensions\social\helpers\gravatars;
+use \ntentan\views\helpers\Helper;
+
+class GravatarsHelper extends Helper {
+
+    private $hash;
+    private $size = 48;
+
+    public function help($email) 
+    {
+        $this->hash = md5(strtolower(trim($email)));
+        return $this;
+    }
+
+    public function size($size) 
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    public function __toString() 
+    {
+        return "http://www.gravatar.com/avatar/{$this->hash}.jpg?s={$this->size}&amp;d=mm";
+    }
+
+}
