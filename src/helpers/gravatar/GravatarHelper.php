7b47fe52a241fc1273b31fdd5120dcdb5bfb511e
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-05-28 10:12:38 +0000

    Added a way to change default avatars in gravatar

--- src/helpers/gravatar/GravatarHelper.php
+++ src/helpers/gravatar/GravatarHelper.php
@@ -2,10 +2,11 @@
 namespace ntentan\extensions\social\helpers\gravatar;
 use \ntentan\honam\Helper;
 
-class GravatarHelper extends Helper {
-
+class GravatarHelper extends Helper 
+{
     private $hash;
     private $size = 48;
+    private $default = 'identicon';
 
     public function help($email) 
     {
@@ -18,10 +19,15 @@ class GravatarHelper extends Helper {
         $this->size = $size;
         return $this;
     }
+    
+    public function setDefault($default)
+    {
+        $this->default = $default;
+    }
 
     public function __toString() 
     {
-        return "http://www.gravatar.com/avatar/{$this->hash}.jpg?s={$this->size}&amp;d=mm";
+        return "http://www.gravatar.com/avatar/{$this->hash}.jpg?s={$this->size}&amp;d={$this->default}";
     }
 
 }
